8460d42b6de23fa6cec2dd7a15314290227593d1
Rustamxxd <125432441+Rustamxxd@users.noreply.github.com>
2024-04-07 18:50:57 +0600

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import './styles/style.scss'
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Students from './pages/Students/Students';
+import Student from './pages/Student/Student';
+
+function App() {
+  return (
+    <Routes>
+      <Route path={"/"} element={<Layout />}>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/about"} element={<About />} />
+        <Route path={"/contact"} element={<Contact />} />
+        <Route path={"/students"} element={<Students />} />
+        <Route path={"/student/:id"} element={<Student />} />
+      </Route>
+    </Routes>
+  );
+}
+export default App;
